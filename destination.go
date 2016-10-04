72878a98975e4f6b98d638a5e0680db637925044
nikhilR <nikhil.raman@zerodha.com>
2016-10-04 19:51:36 +0530

    Add Disconnect()

--- destination.go
+++ destination.go
@@ -2,31 +2,34 @@ package goaudio
 
 import "fmt"
 
-type Destination struct{
-	
+type Destination struct {
 	node Node
 }
 
-func (d *Destination) Connect(c Component){
-	
+func (d *Destination) Connect(c Component) {
+
 	fmt.Println(c)
-	
+
 }
 
-func (d *Destination) process(){
-	
+func (d *Destination) Disconnect() ([]Component, []Component) {
+	return nil, nil
+}
+
+func (d *Destination) process() {
+
 	d.node.tickCount = d.node.tickCount + 1
 	//fmt.Println(d.node.buffer)
 	//Clear the buffer of the previous cycle values
 	copy(d.node.buffer, emptyBuffer)
-	
+
 	//Process immediate input nodes. The nodes handle their own processes independently on their inputs
 	for _, comp := range d.node.input {
 		compnode := comp.getNode()
 		if d.node.tickCount > compnode.tickCount {
-		comp.process()
+			comp.process()
 		}
-		
+
 		//accumulate the values per cycle
 		for i := range d.node.buffer {
 			d.node.buffer[i] = d.node.buffer[i] + compnode.buffer[i]
@@ -34,6 +37,6 @@ func (d *Destination) process(){
 	}
 }
 
-func (d *Destination) getNode() *Node{
+func (d *Destination) getNode() *Node {
 	return &d.node
 }
