1087cc18eb29e58c46863a5e3497897ae75aea5f
nikhilR <nikhil.raman@zerodha.com>
2016-10-04 19:52:12 +0530

    Refactor. Fixes #1

--- examples/audiobuffertest2.go
+++ examples/audiobuffertest2.go
@@ -1,39 +1,38 @@
 /*Still highly experimental
  * Press any key to exit, hahaha
  * Check README.md
- * 
+ *
  */
 
 //
-
 
 // same example as audiobuffertest.go only this one uses AudioContext.CreateBuffer to create a buffer from a wav file
 package main
 
 import "github.com/mrnikho/goaudio"
 import "fmt"
-import "math/rand"
-func main(){
-	
-	context:= goaudio.NewAudioContext(44100)
-	
+
+func main() {
+
+	context := goaudio.NewAudioContext(44100)
+
 	fmt.Println("The Sampling rate is: ", context.GetSampleRate())
-	
+
 	audiosource := context.CreateBufferSource()
-	
+
 	//Create buffer returns the audio data as a []float32 slice and the sampling rate
 	data, sr := context.CreateBuffer("piano.wav")
 	fmt.Println("The Sampling rate is: ", sr, " Hz.")
 	audiosource.Buffer = &data
-	
+
 	audiosource.Connect(context.Dest)
 	audiosource.Loop = true
 	audiosource.Start()
-	context.Play()	
-    
-   	//blocking the main from returning and exiting the program; press any key to exit
-    var input string
-    fmt.Scanln(&input)
-    fmt.Print("Done.")
-	
+	context.Play()
+
+	//blocking the main from returning and exiting the program; press any key to exit
+	var input string
+	fmt.Scanln(&input)
+	fmt.Print("Done.")
+
 }
--- examples/test.go
+++ examples/test.go
@@ -1,7 +1,7 @@
 /*Still highly experimental
  * Press Any Key to exit, hahaha
  * Check README.md
- * 
+ *
  */
 
 package main
@@ -10,57 +10,55 @@ import "github.com/mrnikho/goaudio"
 import "fmt"
 import "time"
 
-func main(){
-	
+func main() {
+
 	//create audio context
 	context := goaudio.NewAudioContext(44100)
-	
+
 	fmt.Println(context.GetSampleRate())
-	
+
 	var freq, detune float32
-	
+
 	freq = 110
 	osctype := "SINE"
 	detune = 0
-	
+
 	//Create oscillator
 	osc := context.CreateOscillator(freq, osctype, detune)
-	
+
 	//Gain
 	gain := context.CreateGain()
 	gain.Gain.Value = 0.2
-	
+
 	//Destination
 	dest := context.Dest
-	
+
 	//Connections similar to webaudio api
 	osc.Connect(gain)
 	gain.Connect(dest)
 	//This initialises the portaudio stream and calls process recursively starting from the destination
 	context.Play()
 
-	osc.Start(4)	
-	
+	osc.Start()
+
 	time.Sleep(time.Second * 2) // 2 sec
 	gain.Gain.Value = 0.4
 	time.Sleep(time.Second * 2) // 4 sec
 	gain.Gain.Value = 0.6
-	
-	
+
 	osc.OscType = "SAW"
-	
+
 	time.Sleep(time.Second * 2) // 6 sec
 	gain.Gain.Value = 0.8
 	time.Sleep(time.Second * 2) // 8 sec
 	osc.Frequency.Value = 440
 	gain.Gain.Value = 0.4
 	time.Sleep(time.Second * 2) //10 sec
-	gain.Gain.Value = 0  // shhh
-	
-	
+	gain.Gain.Value = 0         // shhh
+
 	//blocking the main from returning and exiting the program; press any key to exit
 	var input string
-    fmt.Scanln(&input)
-    fmt.Print("Done.")
+	fmt.Scanln(&input)
+	fmt.Print("Done.")
 
 }
